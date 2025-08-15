dd5ac8f8c00429e7f95f8f72a4211728108d9fcf
Achacu <eloicobres@gmail.com>
2025-08-15 02:49:03 +0100

    Added erf function (will be used to calculate gaussian density)

--- src/D3D12RaytracingSimpleLighting/Raytracing.hlsl
+++ src/D3D12RaytracingSimpleLighting/Raytracing.hlsl
@@ -85,6 +85,42 @@ float4 CalculateDiffuseLighting(float3 hitPosition, float3 normal)
 
     return g_cubeCB.albedo * g_sceneCB.lightDiffuseColor * fNDotL;
 }
+
+//https://gpuopen.com/download/Accurate_Diffuse_Lighting_from_Spherical_Gaussian_Lights_(supplemental).pdf
+float erf(float x)
+{
+    // Early return for large |x|.
+    if (abs(x) >= 4.0)
+        return asfloat((asuint(x) & 0x80000000) ^ asuint(1.0)); //bitwise AND and XOR operators
+    
+    // Polynomial approximation based on https://forums.developer.nvidia.com/t/optimized-version-of-single-precision-error-function-erff/40977
+    if (abs(x) > 1.0)
+    {
+        float A1 = 1.628459513;
+        float A2 = 9.15674746e-1;
+        float A3 = 1.54329389e-1;
+        float A4 = -3.51759829e-2;
+        float A5 = 5.66795561e-3;
+        float A6 = -5.64874616e-4;
+        float A7 = 2.58907676e-5;
+        float a = abs(x);
+        float y = 1.0 - exp2(-(((((((A7 * a + A6) * a + A5) * a + A4) * a + A3) * a + A2) * a + A1) * a));
+        return asfloat((asuint(x) & 0x80000000) ^ asuint(y));
+    }
+    else
+    {
+        float A1 = 1.128379121;
+        float A2 = -3.76123011e-1;
+        float A3 = 1.12799220e-1;
+        float A4 = -2.67030653e-2;
+        float A5 = 4.90735564e-3;
+        float A6 = -5.58853149e-4;
+        float x2 = x * x;
+        return (((((A6 * x2 + A5) * x2 + A4) * x2 + A3) * x2 + A2) * x2 + A1) * x;
+    }
+}
+
+
 float CalculateTransmittance(float3 r0, float3 rd, Intersection intersection)
 {
     float3 p = r0 + intersection.tIn * rd;
